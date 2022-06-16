c76d267d8853ce1a958f4f1692af25323e306b9e
heartbans <52736090+heartbans@users.noreply.github.com>
2022-06-16 14:48:42 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# V2Ray generate configuration
-# Download and install V2Ray
+# generate configuration
+# Download and install
 config_path=$PROTOCOL"_ws_tls.json"
 mkdir /tmp/v2ray
 curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
@@ -9,15 +9,10 @@ install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v2ray
 install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
 # Remove temporary directory
 rm -rf /tmp/v2ray
-# V2Ray new configuration
+# new configuration
 install -d /usr/local/etc/v2ray
 envsubst '\$UUID,\$WS_PATH' < $config_path > /usr/local/etc/v2ray/config.json
-# MK TEST FILES
-mkdir /opt/test
-cd /opt/test
-dd if=/dev/zero of=100mb.bin bs=100M count=1
-dd if=/dev/zero of=10mb.bin bs=10M count=1
-# Run V2Ray
+# Run
 /usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json &
 # Run nginx
 /bin/bash -c "envsubst '\$PORT,\$WS_PATH' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf" && nginx -g 'daemon off;'
