ca891ee87665c3db12c5d8c6fac9a894a554cfd6
heartbans <52736090+heartbans@users.noreply.github.com>
2022-06-21 12:11:07 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # generate configuration
 # Download and install
-config_path=$PROTOCOL"_ws_tls.json"
+config_path="config_vm.json"
 mkdir /tmp/v2ray
 curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
 unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
